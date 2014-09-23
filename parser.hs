0c51afd0edec17268ec18010325002f83452c024
sh0hei <vividbit@gmail.com>
2014-09-23 20:59:22 +0900

    Add some parse*

--- parser.hs
+++ parser.hs
@@ -1,4 +1,5 @@
 import Control.Monad
+import Numeric
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -23,11 +24,52 @@ data LispVal = Atom String
              | String String
              | Bool Bool
 
+parseBool :: Parser LispVal
+parseBool = do string "#"
+               x <- oneOf "tf"
+               return $ case x of
+                          't' -> Bool True
+                          'f' -> Bool False
+
+parseNumber :: Parser LispVal
+parseNumber =  do num <- parseDigital1 <|> parseDigital2 <|> parseHex <|> parseOct <|> parseBin
+                  return $ num
+
 parseString :: Parser LispVal
 parseString = do char '"'
                  x <- many $ escapedChars <|> noneOf "\"\\"
                  char '"'
                  return $ String x
+
+parseDigital1 :: Parser LispVal
+parseDigital1 = do x <- many1 digit
+                   (return . Number . read) x
+
+parseDigital2 :: Parser LispVal
+parseDigital2 = do try $ string "#d"
+                   x <- many1 digit
+                   (return . Number . read) x
+
+parseHex :: Parser LispVal
+parseHex = do try $ string "#x"
+              x <- many1 hexDigit
+              return $ Number (hex2dig x)
+
+parseOct :: Parser LispVal
+parseOct = do try $ string "#o"
+              x <- many1 octDigit
+              return $ Number (oct2dig x)
+
+parseBin :: Parser LispVal
+parseBin = do try $ string "#b"
+              x <- many1 (oneOf "10")
+              return $ Number (bin2dig x)
+
+oct2dig x = fst $ readOct x !! 0
+hex2dig x = fst $ readHex x !! 0
+bin2dig  = bin2dig' 0
+bin2dig' digint "" = digint
+bin2dig' digint (x:xs) = let old = 2 * digint + (if x == '0' then 0 else 1) in bin2dig' old xs
 
 parseAtom :: Parser LispVal
 parseAtom = do first <- letter <|> symbol
@@ -38,13 +80,11 @@ parseAtom = do first <- letter <|> symbol
                           "#f" -> Bool False
                           _    -> Atom atom
 
-parseNumber :: Parser LispVal
-parseNumber = many1 digit >>= return . Number . read
-
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
         <|> parseNumber
+        <|> parseBool
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
